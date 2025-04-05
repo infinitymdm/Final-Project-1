a2cc3e1df9dd038c6468c9f1cfa918dbb6a6b804
andrew-ash <andrew.ash@eagles.oc.edu>
2025-04-05 14:17:58 -0500

    Add name==main thing

--- Code/scripts/wav2vec2_model.py
+++ Code/scripts/wav2vec2_model.py
@@ -12,19 +12,20 @@ class Filler_Detector(nn.Module):
         pooled = features.mean(dim=1) # This is just global average pooling, it allows for different number of time steps if needed
         return self.classifier(pooled).squeeze(1) # This makes sure the output is not multidimensional
 
-ONLY_FINAL_LAYER_TRAINING = False
+if __name__ == "__main__":
+    ONLY_FINAL_LAYER_TRAINING = False
 
-bundle = pipelines.WAV2VEC2_BASE
-model = bundle.get_model()
+    bundle = pipelines.WAV2VEC2_BASE
+    model = bundle.get_model()
 
-#print(model)
-#print(bundle._params['encoder_embed_dim'])
+    #print(model)
+    #print(bundle._params['encoder_embed_dim'])
 
-FINAL_LAYER_IN = bundle._params['encoder_embed_dim']
+    FINAL_LAYER_IN = bundle._params['encoder_embed_dim']
 
-if ONLY_FINAL_LAYER_TRAINING:
-    for params in model.parameters():
-        params.requires_grad = False
+    if ONLY_FINAL_LAYER_TRAINING:
+        for params in model.parameters():
+            params.requires_grad = False
 
-fd_model = Filler_Detector(model, FINAL_LAYER_IN)
-print(fd_model)
+    fd_model = Filler_Detector(model, FINAL_LAYER_IN)
+    print(fd_model)
