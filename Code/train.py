b39ca6f664b7e0dacca731e106c0e86e439ddea9
Marcus Mellor <infinitymdm@gmail.com>
2025-04-09 16:59:36 -0500

    train.py: Initial work supporting model save

--- Code/train.py
+++ Code/train.py
@@ -8,32 +8,23 @@ import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-def train(model, dataset, loss_fn, opt_fn, **hyperparams):
+def train(model, train_data, loss_fn, optimizer, **hyperparams):
     '''Return a model trained on the dataset using the specified hyperparameters.
 
     Arguments:
         model:      a torch.nn.Module object to be trained
-        dataset:    a torch.utils.data.Dataset to be sampled for training data and labels
-        loss_fn:    the loss function (e.g. `torch.nn.CrossEntropyLoss()`) to use in training
-        opt_fn:     the optimizer function (e.g. `torch.optim.Adam`) to use in training
+        dataset:    a torch.utils.data.DataLoader to be sampled for training data and labels
+        loss_fn:    the loss function (e.g. `torch.nn.CrossEntropyLoss) to use in training
+        optimizer:  the optimizer (e.g. `torch.optim.Adam`) to use in training
 
     Keyword arguments
-        batch_size: (defaults to 8)
-        learn_rate: (defaults to 1e-3)
         num_epochs: (defaults to 1)
         device:     (defaults to cuda if available)
     '''
-    batch_size = hyperparams.get('batch_size', 8)
-    learn_rate = hyperparams.get('learn_rate', 1e-3)
     num_epochs = hyperparams.get('num_epochs', 1)
     device = hyperparams.get('device', torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
-
-    # Initialize a loader for the dataset
-    train_data = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-    # Initialize the optimizer with the model parameters (on the appropriate device)
     model.to(device)
-    optimizer = opt_fn(model.parameters(), lr=learn_rate)
+    model = torch.compile(model)
 
     # Each epoch, iterate over data
     for epoch in range(num_epochs):
@@ -57,7 +48,7 @@ def train(model, dataset, loss_fn, opt_fn, **hyperparams):
                 progress_bar.update(1)
                 progress_bar.set_postfix_str(f'Test Loss: {(avg_loss/i):.5f}')
 
-    return model
+    return model, optimizer
 
 
 def test_fbeta(model, dataset, **hyperparams):
@@ -65,19 +56,14 @@ def test_fbeta(model, dataset, **hyperparams):
 
     Arguments:
         model:      a torch.nn.Module to be tested
-        dataset:    a torch.utils.data.Dataset to be sampled for test data and labels
+        dataset:    a torch.utils.data.DataLoader to be sampled for test data and labels
 
     Keyword arguments:
-        batch_size: (defaults to 8)
-        device:     (defaults to cuda if available)
         beta:       (defaults to 1)
+        device:     (defaults to cuda if available)
     '''
-    batch_size = hyperparams.get('batch_size', 8)
     device = hyperparams.get('device', torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
     beta = hyperparams.get('beta', 1)
-
-    # Initialize a loader for the dataset
-    test_data = DataLoader(dataset, batch_size=batch_size)
 
     # Initialize the fbeta metric to average over the test dataset
     fbeta = Fbeta(beta=beta, average=True)
@@ -105,22 +91,39 @@ def test_fbeta(model, dataset, **hyperparams):
 
 if __name__ == "__main__":
     # Hyperparameters
-    batch_size = 32
+    batch_size = 16
+    learn_rate = 1e-3
+    num_epochs = 1
+    loss_fn = torch.nn.CrossEntropyLoss()
+    opt_fn = torch.optim.Adam
 
     # Initialize train and test datasets
     pcf_root = Path('data/PodcastFillers')
     pcf_csv = pcf_root / 'metadata' / 'PodcastFillers.csv'
     pcf_wav_dir = pcf_root / 'audio' / 'clip_wav'
     pcf_dataset = lambda s: PodcastFillersDataset(pcf_csv, pcf_wav_dir, split=s)
+    train_data = DataLoader(pcf_dataset('train'), batch_size=batch_size, shuffle=True)
+    test_data = DataLoader(pcf_dataset('test'), batch_size=batch_size)
 
-    # Initialize the model
-    model = torch.compile(FillerDetector())
+    # Initialize the model and optimizer
+    model = FillerDetector()
+    optimizer = opt_fn(model.parameters(), lr=learn_rate)
+
+    # If loading a previous checkpoint, set ckpt_name to the filepath
+    ckpt_name = 'model_0.831.ckpt'
+    if Path(ckpt_name).exists():
+        checkpoint = torch.load(ckpt_name, weights_only=True)
+        model = model.load_state_dict(checkpoint['model'], strict=False)
+        optimizer = optimizer.load_state_dict(checkpoint['optimizer'])
 
     # Train the model
-    model = train(model, pcf_dataset('train'), torch.nn.CrossEntropyLoss(), torch.optim.Adam, batch_size=batch_size)
+    model, optimizer = train(model, train_data, loss_fn, optimizer, num_epochs=num_epochs)
 
     # Test the model
-    score = test_fbeta(model, pcf_dataset('test'), beta=0.5, batch_size=batch_size)
+    score = test_fbeta(model, test_data, beta=0.5)
 
-    # Save the model to a file
-    torch.save(model.state_dict(), f'model_{score}.pt')
+    # Save a checkpoint
+    torch.save({
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict()
+    }, f'model_{score:.3f}.ckpt')
