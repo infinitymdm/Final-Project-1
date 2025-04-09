445f51382fdcf7127adfdffe5e5e3c0617b4b24f
Marcus Mellor <infinitymdm@gmail.com>
2025-04-08 21:27:14 -0500

    Code/scripts: Fix bugs in dataset download script

--- Code/scripts/download_dataset.sh
+++ Code/scripts/download_dataset.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 
 # Fetch and assemble the dataset if we don't have the full zip
-if [ -f "PodcastFillers-full.zip" ]; then
+if [ ! -f "PodcastFillers-full.zip" ]; then
     # Download the files for PodcastFillers from the hosting service.
     wget https://zenodo.org/records/7121457/files/PodcastFillers.csv
     wget https://zenodo.org/records/7121457/files/PodcastFillers.z01
@@ -17,4 +17,5 @@ fi
 unzip PodcastFillers-full.zip
 
 # Move the dataset into Code/data
+mkdir ../data
 mv PodcastFillers ../data/.
