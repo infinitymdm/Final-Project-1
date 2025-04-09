cddf519de265bf46ab02fce1d864085a0d63c90c
Marcus Mellor <infinitymdm@gmail.com>
2025-04-09 10:43:06 -0500

    Code/train.py: Move training loop into a function

--- Code/train.py
+++ Code/train.py
@@ -7,19 +7,53 @@ import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
+def train(model, dataset, loss_fn, opt_fn, **hyperparams):
+    '''Return a model trained on the dataset using the specified hyperparameters.
+
+    Arguments:
+        model:      a torch.nn.Module object to be trained
+        dataset:    a torch.utils.data.Dataset to be sampled for training data and labels
+        loss_fn:    the loss function (e.g. `torch.nn.CrossEntropyLoss()`) to use in training
+        opt_fn:     the optimizer function (e.g. `torch.optim.Adam`) to use in training
+
+    Keyword arguments
+        batch_size: (defaults to 8)
+        learn_rate: (defaults to 1e-3)
+        num_epochs: (defaults to 1)
+        device:     (defaults to cuda if available)
+    '''
+    batch_size = hyperparams.get('batch_size', 8)
+    learn_rate = hyperparams.get('learn_rate', 1e-3)
+    num_epochs = hyperparams.get('num_epochs', 1)
+    device = hyperparams.get('device', torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+
+    # Initialize the optimizer with the model parameters (on the appropriate device)
+    model.to(device)
+    optimizer = opt_fn(model.parameters(), lr=learn_rate)
+
+    # Main training loop
+    for epoch in range(num_epochs):
+        with tqdm(total=len(train_data), desc=f'Epoch {epoch}') as progress_bar:
+            for i, batch in enumerate(train_data):
+                data, targets = batch
+                data.to(device)
+                targets.to(device)
+
+                # Perform one training step & calculate the gradient
+                optimizer.zero_grad()
+                predictions = model(data)
+                loss = loss_fn(predictions, targets)
+                loss.backward()
+                optimizer.step()
+
+                # Show progress
+                progress_bar.update(1)
+                progress_bar.set_postfix_str(f'Test Loss: {loss:.5f}')
+
+    return model
 
 
 if __name__ == "__main__":
-    # Hyperparameters
-    batch_size = 8
-    learn_rate = 1e-3
-    num_epochs = 1
-    criterion = torch.nn.CrossEntropyLoss()
-    opt_function = torch.optim.Adam
-
-    # Set up pytorch to use hardware acceleration (if available)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
     # Initialize train and test datasets
     pcf_root = Path('data/PodcastFillers')
     pcf_csv = pcf_root / 'metadata' / 'PodcastFillers.csv'
@@ -28,23 +62,11 @@ if __name__ == "__main__":
     train_data = DataLoader(pcf_dataset('train'), batch_size=batch_size, shuffle=True)
     test_data = DataLoader(pcf_dataset('test'), batch_size=batch_size)
 
-    # Initialize the model and optimizer
-    model = FillerDetector()
-    optimizer = opt_function(model.parameters(), lr=learn_rate)
+    # Initialize the model
+    model = FillerDetector(train_transfer_model)
 
     # Train the model
-    for epoch in range(num_epochs):
-        with tqdm(total=len(train_data), desc=f'Epoch {epoch}') as progress_bar:
-            for i, data in enumerate(train_data):
-                inputs, labels = data
+    train(model, train_data, torch.nn.CrossEntropyLoss(), torch.optim.Adam, batch_size=32)
 
-                # Perform one training step & calculate the gradient
-                optimizer.zero_grad()
-                outputs = model(inputs)
-                loss = criterion(outputs, labels)
-                loss.backward()
-                optimizer.step()
-
-                # Show progress
-                progress_bar.update(1)
-                progress_bar.set_postfix_str(f'Test Loss: {loss:.5f}')
+    # Save the model to a file
+    torch.save(model.state_dict(), "model.pt")
