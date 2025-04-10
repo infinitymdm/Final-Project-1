285d23c7a3f83236cea74d0d58d8774fee47927f
Marcus Mellor <infinitymdm@gmail.com>
2025-04-10 17:04:17 -0500

    Code/audio_utils: test basic async audio input

--- Code/audio_utils.py
+++ Code/audio_utils.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import asyncio
 import sounddevice as sd
 # See https://python-sounddevice.readthedocs.io/en/0.5.1/usage.html#callback-streams for info
 # on real-time recording & playback
@@ -19,5 +20,38 @@ def downsample(audio, sample_rate, new_sample_rate):
     pass
 
 def convert_to_tensors(audio, sample_rate, shape):
-    '''Convert input audio to tensors of the desired shape. Returns a list of tensors'''
+    '''Convert input audio (arbitrary length) to tensors of the desired shape. Returns a list of tensors'''
     pass
+
+async def audiostream(channels=1, **kwargs):
+    '''Generator yielding blocks of input audio data'''
+    q_in = asyncio.Queue()
+    loop = asyncio.get_event_loop()
+
+    def callback(data, frame_count, time_info, status):
+        loop.call_soon_threadsafe(q_in.put_nowait, (data.copy(), status))
+
+    stream = sd.InputStream(callback=callback, channels=channels, **kwargs)
+    with stream:
+        while True:
+            data, status = await q_in.get()
+            yield data, status
+
+async def sample_audiostream(**kwargs):
+    '''Asynchronous task to sample data from an audiostream'''
+    async for data, status in audiostream(**kwargs):
+        if status:
+            print(status)
+        print('min:', data.min(), '\tmax:', data.max())
+
+if __name__ == "__main__":
+    async def main():
+        try:
+            await asyncio.wait_for(sample_audiostream(), timeout=2)
+        except asyncio.TimeoutError:
+            pass
+
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        sys.exit('\nInterrupted by user')
