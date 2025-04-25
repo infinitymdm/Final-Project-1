64ff17acebbce929443deef6617caa835025f4c4
andrew-ash <andrew.ash@eagles.oc.edu>
2025-04-25 15:37:13 -0500

    Second day of model exploration: BCE is unstable will need new learning rate

--- Code/train.py
+++ Code/train.py
@@ -7,10 +7,12 @@ from pathlib import Path
 import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, accuracy_score
 import pandas as pd
+from datetime import datetime
 
-def train(model, train_data, loss_fn, optimizer, intermediate_test=None, **hyperparams):
+
+def train(model, train_data, loss_fn, optimizer, intermediate_test=None, BCELoss=False, run_dir=Path(''), **hyperparams):
     '''Return a model trained on the dataset using the specified hyperparameters.
 
     Arguments:
@@ -32,6 +34,7 @@ def train(model, train_data, loss_fn, optimizer, intermediate_test=None, **hyper
     ckpt_dir = Path('ckpt')
     best_score = 0.0
     beta = hyperparams.get('beta', 0.5)
+    epoch_metrics = []
 
     # Each epoch, iterate over data
     for epoch in range(num_epochs):
@@ -41,10 +44,16 @@ def train(model, train_data, loss_fn, optimizer, intermediate_test=None, **hyper
             for i, batch in enumerate(train_data):
                 data, targets = batch
 
+                if BCELoss:
+                    targets = targets.float().unsqueeze(1).to(device)
+
                 # Perform one training step & calculate the gradient
                 optimizer.zero_grad()
                 predictions = model(data.to(device))
-                loss = loss_fn(predictions, targets.to(device))
+                if BCELoss:
+                    loss = CustomBCE(predictions, targets)
+                else:
+                    loss = loss_fn(predictions, targets.to(device))
                 loss.backward()
                 optimizer.step()
 
@@ -55,17 +64,88 @@ def train(model, train_data, loss_fn, optimizer, intermediate_test=None, **hyper
 
         # Save the best performing (f-0.5 score) model at intermediate steps
         if intermediate_test is not None:
-            score, _ = test_fbeta(model, intermediate_test, beta=beta, device=device)
-            if score > best_score:
-                best_score = score
-                ckpt_name = ckpt_dir / f'f{score:.3f}_epoch{epoch}.ckpt'
-                ckpt_name.mkdir(parents=True, exist_ok=True)
-                torch.save(model.state_dict(), ckpt_name / 'model.pt')
-                torch.save(optimizer.state_dict(), ckpt_name / 'optimizer.pt')
+            f05, _, acc, prec, rec = test_metrics(model, intermediate_test, beta=beta, device=device)
+
+            ckpt_name = run_dir / f'f{f05:.3f}_epoch{epoch}'
+            ckpt_name.mkdir(parents=True, exist_ok=True)
+            torch.save(model.state_dict(), ckpt_name / 'model.pt')
+            torch.save(optimizer.state_dict(), ckpt_name / 'optimizer.pt')
+
+            if f05 > best_score:
+                best_score = f05
                 print(f"New best score: {best_score:.3f} saved to {ckpt_name}")
+
+            epoch_metrics.append({
+                "epoch": epoch,
+                "accuracy": acc,
+                "precision": prec,
+                "recall": rec,
+                "f0.5": f05
+            })
+
+    df_metrics = pd.DataFrame(epoch_metrics)
+    df_metrics.to_csv(run_dir / "metrics.csv", index=False)
 
     return model, optimizer
 
+def CustomBCE(logits, targets, fp_weight=1.0,fn_weight=1.0):
+    probs = torch.sigmoid(logits)
+    eps = 1e-6
+    loss = -((fn_weight * targets * torch.log(probs + eps)) + (fp_weight * (1 - targets) * torch.log(1 - probs + eps)))
+    return loss.mean()
+
+def test_metrics(model, dataset, **hyperparams):
+    '''Test a model on the given dataset and return the fbeta score, precision, recall, and accuracy.
+
+        Arguments:
+            model:      a torch.nn.Module to be tested
+            dataset:    a torch.utils.data.DataLoader to be sampled for test data and labels
+
+        Keyword arguments:
+            beta:       (defaults to 1)
+            device:     (defaults to cuda if available)
+            threshold:  (defaults to 0.5)
+        '''
+    device = hyperparams.get('device', torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+    beta = hyperparams.get('beta', 1)
+    threshold = hyperparams.get('threshold', 0.5)
+
+    # Initialize the fbeta metric to average over the test dataset
+    fbeta = Fbeta(beta=beta, average=True)
+
+    all_predictions = []
+    all_targets = []
+
+    # Iterate over data
+    model.eval()
+    with torch.no_grad():
+        with tqdm(total=len(test_data), desc='Testing') as progress_bar:
+            for i, batch in enumerate(test_data):
+                data, targets = batch
+
+                # Test on the batch of data and calculate fbeta score
+                logits = model(data.to(device))
+                # probs in this function is performing a sigmoid under the assumption of a single neuron output layer
+                probs = torch.sigmoid(logits).view(-1).cpu().numpy()
+                pred_batch = (probs > threshold).astype(int)  # Custom threshold if needed
+
+                targ_batch = targets.cpu().numpy()
+                fbeta.update((torch.tensor(pred_batch).to(device), targets.to(device)))
+                score = fbeta.compute()
+
+                all_predictions.extend(pred_batch)
+                all_targets.extend(targ_batch)
+
+                # Show progress
+                progress_bar.update(1)
+                progress_bar.set_postfix_str(f'f{beta}: {score:.5f}')
+
+    accuracy = accuracy_score(all_targets, all_predictions)
+    precision = precision_score(all_targets, all_predictions)
+    recall = recall_score(all_targets, all_predictions)
+
+    cm = confusion_matrix(all_targets, all_predictions)
+    return score, cm, accuracy, precision, recall
 
 def test_fbeta(model, dataset, **hyperparams):
     '''Test a model on the given dataset and return the fbeta score.
@@ -111,16 +191,24 @@ def test_fbeta(model, dataset, **hyperparams):
     cm = confusion_matrix(all_targets, all_predictions)
     return score, cm
 
-
 if __name__ == "__main__":
+    now = datetime.now()
+    run_name = now.strftime("%Y-%m-%d_%H-%M")
+    run_dir = Path("ckpt") / run_name
+    run_dir.mkdir(parents=True, exist_ok=True)
+    print(f"Saving run to: {run_dir}")
+
     # Hyperparameters
     batch_size = 16
     learn_rate = 1e-3
-    num_epochs = 50
+    num_epochs = 1
     opt_fn = torch.optim.Adam
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # The custom loss weights punish false positives twice as much as false negatives so our detector is not likely to
     # constantly "ring the bell" by detecting a filler.
+    # A value of 1.5 in the weight does slightly improve the false positive rate, but over many epochs can actually start to do the opposite
+    # A value of 3.5 in the weight greatly reduces the false positive rate at the cost of doubling the false negative rate
+    # Values in between are some combination of the two tradeoffs. The best performance so far has led to 1/8 positives are false and 1/7 negatives are false
     custom_loss_weights = torch.tensor([3.5, 1.0],device=device)
     loss_fn = torch.nn.CrossEntropyLoss(weight=custom_loss_weights)
 
@@ -133,7 +221,7 @@ if __name__ == "__main__":
     test_data = DataLoader(pcf_dataset('test'), batch_size=batch_size)
 
     # Initialize the model and optimizer
-    model = FillerDetector()
+    model = FillerDetector(out_dim=1)
     model.to(device)
     optimizer = opt_fn(model.parameters(), lr=learn_rate)
 
@@ -147,8 +235,8 @@ if __name__ == "__main__":
         optimizer.load_state_dict(torch.load(ckpt_name / 'optimizer.pt'))
 
     # Train the model & evaluate results
-    model, optimizer = train(model, train_data, loss_fn, optimizer, intermediate_test=test_data, num_epochs=num_epochs)
-    score, cm = test_fbeta(model, test_data, beta=0.5)
+    model, optimizer = train(model, train_data, loss_fn, optimizer, intermediate_test=test_data, BCELoss=True, run_dir=run_dir, num_epochs=num_epochs)
+    score, cm, _, _, _ = test_metrics(model, test_data)
     #print(model)
     print(f"F0.5 Score: {score:.5f}")
     print(pd.DataFrame(
