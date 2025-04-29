2a2ac9abd55898a05db966258cfdf3db75c45fc7
Marcus Mellor <35574527+infinitymdm@users.noreply.github.com>
2025-04-28 19:50:43 -0500

    Code/bertha.py: Add sigmoid to classifier output
    This makes the threshold actually matter

--- Code/bertha.py
+++ Code/bertha.py
@@ -13,7 +13,8 @@ async def classify_audiostream(classifier, torch_device, threshold=0.5, **kwargs
     async for audio, _ in audiostream(**kwargs):
         data = torch.unsqueeze(torch.flatten(torch.from_numpy(audio)), 0)
         outputs = classifier(data.to(torch_device))
-        is_filler = float(outputs) > threshold
+        probs = torch.sigmoid(outputs).view(-1).cpu().numpy()
+        is_filler = float(probs) > threshold
         filler_count += is_filler
         print(f'Filler detected! (total: {filler_count})') if is_filler else None
 
